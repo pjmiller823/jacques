17a55678f79171339dda5aee8bdd9e55d2df4e74
Patrick Miller <pjmiller823@gmail.com>
2017-04-24 16:56:12 -0400

    solved all errors for notes except for has many. generated tags model.

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,6 @@
 class Note < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+
+  has_many :tags
 end
--- db/migrate/20170424193936_create_notes.rb
+++ db/migrate/20170424193936_create_notes.rb
@@ -1,4 +1,4 @@
-class CreateNotes < ActiveRecord::Migration[5.1]
+class CreateNotes < ActiveRecord::Migration[5.0]
   def change
     create_table :notes do |t|
 
