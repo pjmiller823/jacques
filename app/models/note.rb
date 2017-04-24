6534dd60a85aa30af2e8bfff6f788b4923049665
Patrick Miller <pjmiller823@gmail.com>
2017-04-24 19:36:05 -0400

    generated the rest of a note scaffold. Still failing the note save.

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,6 @@ class Note < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
-  has_many :tags
   has_many :taggings
+  has_many :tags, through: :taggings
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ApplicationRecord
   validates :name, presence: true
-  
+
   belongs_to :note
+  has_many :notes, through: :taggings
   has_many :taggings
-  has_many :notes
 end
